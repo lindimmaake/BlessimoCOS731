67030c49d9c2aabac041f637efd4eb6224d9effc
lindiweM <lindimmaake@gmail.com>
2017-08-19 15:11:54 +0200

    updating project

--- Blessimo/src/main/java/com/blessimo/Blessimo/BlessimoController.java
+++ Blessimo/src/main/java/com/blessimo/Blessimo/BlessimoController.java
@@ -3,6 +3,12 @@ package com.blessimo.Blessimo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.persistence.Entity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @Controller
 public class BlessimoController 
