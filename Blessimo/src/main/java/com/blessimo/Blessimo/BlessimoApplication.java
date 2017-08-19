7b19858c09a2fe059cc9d8136af0ca644ccf73ef
lindiweM <lindimmaake@gmail.com>
2017-08-19 13:56:55 +0200

    updating repo

---
+++ Blessimo/src/main/java/com/blessimo/Blessimo/BlessimoApplication.java
@@ -0,0 +1,12 @@
+package com.blessimo.Blessimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlessimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlessimoApplication.class, args);
+	}
+}
